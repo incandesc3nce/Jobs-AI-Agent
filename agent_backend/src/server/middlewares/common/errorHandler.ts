b546149a7c8647d6c98db2966b56d5a062089d1f
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-05-11 21:42:07 +0700

    feat: remove stack trace from error middleware response

--- agent_backend/src/server/middlewares/common/errorHandler.ts
+++ agent_backend/src/server/middlewares/common/errorHandler.ts
@@ -11,6 +11,6 @@ export function errorHandler(
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env['NODE_ENV'] === 'production' ? '🤮' : err.stack,
+    success: false,
   });
 }
