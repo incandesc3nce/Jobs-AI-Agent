8c6bd75e973d3dbb40fafb21f2d86e948b675ecc
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-05-13 23:43:00 +0700

    feat: create temporarily solution for parsing jobs

--- agent_backend/src/server/api/vacancies/fetch/index.ts
+++ agent_backend/src/server/api/vacancies/fetch/index.ts
@@ -1,3 +1,4 @@
+import { agentService } from '@/server/services/AgentService';
 import { vacanciesService } from '@/server/services/VacanciesService';
 import { decorateError } from '@/utils/decorateError';
 import { Request, Response } from 'express';
@@ -8,6 +9,10 @@ export const fetchVacanciesRoute = async (req: Request, res: Response) => {
   const url = `https://api.hh.ru/vacancies?text=${text}&page=${page}&per_page=${per_page}`;
   try {
     const items = await vacanciesService.getHhVacancies(url);
+
+    items.forEach((item) => {
+      agentService.summarizeVacancy(item);
+    });
 
     res.status(200).json({
       items,
