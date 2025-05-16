57ee094c732ad6650d7179691766ffcc1253393b
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-05-16 23:07:29 +0700

    feat: add jwt methods

--- agent_backend/src/lib/express/auth/jwt.ts
+++ agent_backend/src/lib/express/auth/jwt.ts
@@ -1,15 +1,16 @@
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import { decorateError } from '@/utils/decorateError';
+import { Request } from 'express';
 
-export const signJWT = (payload: {name: string}) => {
+export const signJWT = (payload: { name: string }) => {
   const secret = process.env['JWT_SECRET'] as string;
   const token = jwt.sign(payload, secret, {
     expiresIn: '14d',
   });
 
   return token;
-}
+};
 
 export const verifyJWT = (token: string | undefined | null) => {
   if (!token) {
@@ -23,4 +24,29 @@ export const verifyJWT = (token: string | undefined | null) => {
     decorateError(error);
     return null;
   }
-}
+};
+
+export const decryptJWT = (token: string | undefined | null) => {
+  if (!token) {
+    return null;
+  }
+
+  try {
+    const decoded = jwt.decode(token);
+
+    return decoded as { name: string };
+  } catch (error) {
+    decorateError(error);
+    return null;
+  }
+};
+
+export const getJWTFromHeader = (req: Request) => {
+  const token = req.headers.authorization?.replace('Bearer ', '');
+
+  if (!token) {
+    return null;
+  }
+
+  return token;
+};
