56013cd008867683dbb010395dc3e91c3f856af9
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-05-16 23:41:11 +0700

    feat: add resume crud

--- agent_backend/src/server/services/ResumeService.ts
+++ agent_backend/src/server/services/ResumeService.ts
@@ -1,5 +1,5 @@
-import { prisma } from "@/lib/prisma/prisma";
-import { Resume } from "../../../prisma/generated";
+import { prisma } from '@/lib/prisma/prisma';
+import { Resume, WorkFormat } from '../../../prisma/generated';
 
 class ResumeService {
   private static instance: ResumeService;
@@ -33,19 +33,143 @@ class ResumeService {
     return resume;
   }
 
-  public async updateResume(resume: any): Promise<any> {
-    // Implement the logic to update the resume
-    return {};
+  public async createResume(resume: {
+    username: string;
+    title: string;
+    skills: string[];
+    experience: string;
+    location: string;
+    workFormat: WorkFormat;
+  }) {
+    const { username, title, skills, experience, location, workFormat } =
+      resume;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return {
+        message: 'User not found',
+        success: false,
+      };
+    }
+
+    const newResume = await prisma.resume.create({
+      data: {
+        userId: user.id,
+        title,
+        skills,
+        experience,
+        location,
+        workFormat,
+      },
+    });
+
+    return {
+      resume: newResume,
+      message: 'Successfully created resume',
+      success: true,
+    };
   }
 
-  public async createResume(resume: any): Promise<any> {
-    // Implement the logic to create the resume
-    return {};
+  public async updateResume(resume: {
+    username: string;
+    resumeId: string;
+    title: string;
+    skills: string[];
+    experience: string;
+    location: string;
+    workFormat: WorkFormat;
+  }) {
+    const {
+      username,
+      resumeId,
+      title,
+      skills,
+      experience,
+      location,
+      workFormat,
+    } = resume;
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return {
+        message: 'User not found',
+        success: false,
+      };
+    }
+
+    const updatedResume = await prisma.resume.update({
+      where: {
+        id: resumeId,
+        userId: user.id,
+      },
+      data: {
+        title,
+        skills,
+        experience,
+        location,
+        workFormat,
+      },
+    });
+
+    return {
+      resume: updatedResume,
+      message: 'Successfully updated resume',
+      success: true,
+    };
   }
 
-  public async deleteResume(id: string): Promise<any> {
-    // Implement the logic to delete the resume
-    return {};
+  public async deleteResume(id: string, username: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return {
+        message: 'User not found',
+        success: false,
+      };
+    }
+
+    const resume = await prisma.resume.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!resume) {
+      return {
+        message: 'Resume not found',
+        success: false,
+      };
+    }
+
+    if (resume.userId !== user.id) {
+      return {
+        message: 'You are not authorized to delete this resume',
+        success: false,
+      };
+    }
+
+    await prisma.resume.delete({
+      where: {
+        id,
+      },
+    });
+
+    return {
+      message: 'Successfully deleted resume',
+      success: true,
+    };
   }
 }
 
