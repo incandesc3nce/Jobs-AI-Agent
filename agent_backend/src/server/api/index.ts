5f5d46b3ff91fb9b3b51983c89457469efeeae32
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-05-16 22:31:05 +0700

    feat: add resumes to router

--- agent_backend/src/server/api/index.ts
+++ agent_backend/src/server/api/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import { vacanciesRouter } from './vacancies/vacanciesRouter';
 import { authRouter } from './auth/authRouter';
 import { usersRouter } from './users/usersRouter';
+import { resumesRouter } from './resumes/resumesRouter';
 
 export const apiRouter = express.Router();
 
 apiRouter.use('/vacancies', vacanciesRouter);
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', usersRouter);
+apiRouter.use('/resumes', resumesRouter);
