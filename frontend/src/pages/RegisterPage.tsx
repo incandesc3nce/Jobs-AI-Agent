7a7ffe3fad8c7ef3a1be55587452254c84ab38e9
Zainds <beat1097@mail.ru>
2025-05-17 19:53:49 +0700

    Delete irrelevant import

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { authService } from "../services/authService";
-import type { AuthResponse } from "../types/auth";
 import { Briefcase, UserPlus } from "lucide-react";
 
 const RegisterPage: React.FC = () => {
