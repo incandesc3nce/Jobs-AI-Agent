f3e2bb3a182a1a0ea02bab94bb8992991a26b115
KirillXL <kirillhoroshev.04.83@mail.ru>
2025-05-17 09:58:01 +0700

    new file:   ai_model/README.md 	modified:   ai_model/api.py 	modified:   ai_model/main.py 	new file:   ai_model/resume.json 	new file:   ai_model/resume.py 	deleted:    ai_model/short.json 	modified:   ai_model/vacancies.json

--- ai_model/api.py
+++ ai_model/api.py
@@ -1,25 +1,40 @@
-from fastapi import FastAPI, Depends, HTTPException, Header
+from fastapi import FastAPI, Depends, HTTPException, Header, Request
 import os
 from dotenv import load_dotenv
-from vector import retriever
 from main import generate_answer
+from resume import generate_answer_resume
 
 load_dotenv()
 
-API_KEY_CREDITS = {os.getenv("API_KEY"): 5}
-print(API_KEY_CREDITS)
 app = FastAPI()
 
 def verify_api_key(x_api_key: str = Header(None)):
-    credits = API_KEY_CREDITS.get(x_api_key, 0)
-    if credits <= 0:
-        raise HTTPException(status_code=401, detail="Invalid API Key, or no credits")
-
+    # Просто проверка наличия ключа, без кредитов
+    if not x_api_key or x_api_key != os.getenv("API_KEY"):
+        raise HTTPException(status_code=401, detail="Invalid API Key")
     return x_api_key
 
 @app.post("/generate")
-def generate(prompt: str, x_api_key: str = Depends(verify_api_key)):
-    API_KEY_CREDITS[x_api_key] -= 1
-    # Prepare the prompt with the retrieved documents
-    result = generate_answer(prompt)
+def generate(prompt: str):
+    result = generate_answer(prompt + "/no_think")
+    return result
+
+@app.get("/ping")
+def ping():
+    return {"status": "ok"}
+
+
+
+@app.post("/resume")
+async def resume_endpoint(request: Request, res_prompt: str):
+        # Получаем тело запроса как байты и преобразуем в строку
+    body_bytes = await request.body()
+    body_text = body_bytes.decode("utf-8")
+
+    # Объединяем answer из параметров и тело запроса
+    resume = res_prompt + body_text + '/no_think'
+
+    # Вызываем вашу функцию обработки
+    result = generate_answer_resume(resume)
+
     return result
--- ai_model/resume.py
+++ ai_model/resume.py
@@ -1,16 +1,17 @@
+import pandas as pd
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from vector import retriever
 
+
 model = OllamaLLM(model="qwen3:14b", max_tokens=2000)
 
 template = '''
-You're API to help you find jobs, jobs on the site. Your task is to answer a job seeker's question,\n
- give him role-relevant answers from chroma and attach links to suitable vacancies
+You're an artificial intelligence model that helps clients find role-relevant jobs based on their resumes.
 
-Here are some relevant reviews: {job_reviews}
+Here are some relevant job reviews: {job_reviews}
 
-Here is the question to answer: {question}
+Here is the question to answer: {resume}
 
 '''
 
@@ -18,18 +19,20 @@ prompt = ChatPromptTemplate.from_template(template)
 
 chain = prompt | model
 
-def generate_answer(question: str) -> str:
-    job_reviews = retriever.invoke(question)
-    result = chain.invoke({'job_reviews': job_reviews, 'question': question})
+
+
+def generate_answer_resume(resume: str) -> str:
+    job_reviews = retriever.invoke(resume)
+    result = chain.invoke({'job_reviews': job_reviews,'resume': resume})
     return result
 
 # Для ручного теста из консоли
-if __name__ == "__main__":
+if __name__ == "__resume__":
     while True:
         print("\n\n--------------------------------------------------------------")
         question = input("Enter job descriptions (comma-separated, q to quit): ")
         print("\n\n--------------------------------------------------------------")
         if question.lower() == 'q':
             break
-        answer = generate_answer(question)
+        answer = generate_answer_resume(question)
         print(answer)
